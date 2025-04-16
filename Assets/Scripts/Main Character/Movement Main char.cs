47f1b7bc149908327f2ce7456b1f8e4104a9c5e3
Jose Luis Cordova <134427828+JLCordova07@users.noreply.github.com>
2025-04-16 12:30:31 -0500

    Avance en movimiento de personaje
    Se implementó:
    - Control de movimiento horizontal y vertical.
    - Animación de movimiento horizontal (corriendo).
    
    Pendiente:
    - Animaciones adicionales (salto, caída, mecánica para sostenerse en paredes).
    
    - Mejorar la lógica de los estados de movimiento del personaje.

--- Assets/Scripts/Main Character/Movement Main char.cs
+++ Assets/Scripts/Main Character/Movement Main char.cs
@@ -1,50 +1,88 @@
-using System.Security.Cryptography;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class MovementMainchar : MonoBehaviour
 {
-    //Public variables declaration (can be changed directly at runtime to adjust behavior).
-    public int HorizontalVelocity = 1;
-    public int JumpHeight = 1;
+    //Variables used to adjust movement.
+    [SerializeField] int SpeedParameter = 1;
+    [SerializeField] float JumpParameter = 50f;
+    [SerializeField] int MaxNumberOfJumps = 3;
 
-    //Non-public variables declaration (for internal reference and property modification only).
+    //Public variables using in animation.
+    public bool isRunning;
+    public bool hasJumped;
+    public bool isPropelledUpwards;
+    public bool isFalling;
+    public bool isWallGrabbing;
+
+    //Variables for internal reference and property modification. Make them public to show in Unity Editor.
+    Animator thisCharAnimator;
     Rigidbody2D thisCharRigidBody;
     Vector2 rigidBodyNewVelocity;
-    bool keyWasPressed = false;
+    Vector2 inputValue;
+    SpriteRenderer thisCharSpriteRenderer;
+
+    //Made public for runtime debugging.
+    public int currentJumpsUsed = 0;
+
+    public float currentYVelocity;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         //Initialization of variables.
+        thisCharAnimator = GetComponent<Animator>();
         thisCharRigidBody = GetComponent<Rigidbody2D>();
+        thisCharSpriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (keyWasPressed)
+        UpdateAnimatorStateVariables();
+        Run();
+        Jump();
+        ResetStateVariables();
+        currentYVelocity = thisCharRigidBody.linearVelocityY;
+    }
+    void UpdateAnimatorStateVariables()
+    {
+        thisCharAnimator.SetBool("Is Running", isRunning);
+        thisCharAnimator.SetFloat("Vertical Velocity", thisCharRigidBody.linearVelocityY);
+    }
+    void ResetStateVariables()
+    {
+    }
+    void Run()
+    {
+        if (inputValue.x != 0)
+            thisCharSpriteRenderer.flipX = inputValue.x > 0 ? false : true;
+        rigidBodyNewVelocity = new Vector2(inputValue.x * SpeedParameter, thisCharRigidBody.linearVelocityY);
+        thisCharRigidBody.linearVelocity = rigidBodyNewVelocity;
+    }
+    void Jump()
+    {
+        if (hasJumped)
         {
-            thisCharRigidBody.linearVelocity = rigidBodyNewVelocity;
-            keyWasPressed = false;
+            thisCharRigidBody.linearVelocity += new Vector2(0f, JumpParameter);
+            hasJumped = false;
         }
     }
     void OnJump()
     {
-        keyWasPressed = true;
-        rigidBodyNewVelocity = thisCharRigidBody.linearVelocity;
-        rigidBodyNewVelocity.y = JumpHeight;
-    }
-    void OnMoveleft()
-    {
-        keyWasPressed = true;
-        rigidBodyNewVelocity = thisCharRigidBody.linearVelocity;
-        rigidBodyNewVelocity.x = HorizontalVelocity;
+        if (currentJumpsUsed < MaxNumberOfJumps)
+        {
+            hasJumped = true;
+            isPropelledUpwards = true;
+            currentJumpsUsed++;
+        }
     }
 
-    void OnMoveright()
+    void OnMove(InputValue value)
     {
-        keyWasPressed = true;
-        rigidBodyNewVelocity = thisCharRigidBody.linearVelocity;
-        rigidBodyNewVelocity.x = HorizontalVelocity * (-1);
+        inputValue = value.Get<Vector2>();
+        if (inputValue.x == 0)
+            isRunning = false;
+        else
+            isRunning = true;
     }
 }
